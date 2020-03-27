7e0eb528fd21b9c7913d0d412ba595c7aa1510a0
Viktoriya Kass <vikavika233@gmail.com>
2020-03-28 00:01:05 +0400

    deleted empty rows

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let num = 266219;
 let ar = [];
 let m = 1;
 
-
 for (let i = 0; i < 6; i++) {
     ar[i] = num % 10;
     num = Math.trunc( num / 10 );        
@@ -16,7 +15,6 @@ console.log('Произведение чисел числа 266219: ', m);
 
 m**=3;
 console.log('Возвела в степень 3: ', m);
-
 
 m = m + ''; 
 console.log('Первые 2 цифры числа: ', m.substring(0 , 2) );
